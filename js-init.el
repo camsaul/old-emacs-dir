29a3f18fc0329bb632e2d942e9f32027a93357aa
Cam Saul <cam@geotip.com>
2014-06-02 17:05:49 -0700

    js2-mode to edit javascript

--- js-init.el
+++ js-init.el
@@ -1,18 +1,23 @@
-(provide 'js-init)
+(require 'js2-mode)
 
 (defun cam-js-mode-setup ()
   (global-mode-setup)
   (pretty-function)
-  (electric-layout-m))
+  (electric-layout-mode)
+  )
 
-(add-hook 'js-mode-hook 'cam-js-mode-setup)
+(add-to-list 'auto-mode-alist '("\.js" . js2-mode)) ; use js2-mode instead of js-mode
 
-;; turns the word "function" into a fancy f symbol
+(add-hook 'js2-mode-hook 'cam-js-mode-setup)
+
 (defun pretty-function ()
+  "turns function into a fancy f symbol"
   (font-lock-add-keywords
-   'js-mode `(("\\(function *\\)("
-	       (0 (progn (compose-region (match-beginning 1)
-					 (match-end 1) "\u0192")
-			 nil))))))
+   nil `(("\\(\\<function\\>\\)"
+	  (0 (progn (compose-region (match-beginning 1)
+				    (match-end 1)
+				    "\u0192"
+				    'decompose-region)))))))
 
 (setq js-indent-level 2)
+(provide 'js-init)
