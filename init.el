068cdc3c0008f9baf487045c033263d74fb099b6
Cam Saul <cam@geotip.com>
2014-08-01 13:19:28 -0700

    organize init.el a bit

--- init.el
+++ init.el
@@ -3,7 +3,7 @@
 
 ;; MELPA Package Source
 (require 'package)
-(require 'cl)
+;; (require 'cl)
 (mapc (lambda (l) (add-to-list 'package-archives l))
       '(("melpa" . "http://melpa.milkbox.net/packages/")
 	("marmalade" . "http://marmalade-repo.org/packages/")))
@@ -72,24 +72,25 @@
         xmlgen
         yasnippet))
 
-(mapc 'require '( ;; evil
-		 cam-functions
-		 dired+
-		 midnight
-		 nav				  ; nav frame, better than speed bar
-		 rainbow-delimiters
-		 recentf
-		 smex				  ; IDO-like completion for M-x
-		 undo-tree
+(mapc 'require '(
+                 ;; evil
                  ace-jump-buffer
                  ace-jump-mode
                  bm
+                 cam-functions
+                 dired+
                  find-things-fast
                  flx-ido
                  highlight-symbol
                  loccur
+                 midnight
                  multiple-cursors
+                 nav		 ; nav frame, better than speed bar
+                 rainbow-delimiters
                  rainbow-mode
+                 recentf
+                 smex		 ; IDO-like completion for M-x
+                 undo-tree
 		 ))
 
 ;; (add-hook 'before-make-frame-hook 'turn-off-tool-bar)
@@ -140,120 +141,108 @@
     nil '(("\\<\\(FIX\\(ME\\)?\\|TODO\\|HACK\\|REFACTOR\\|DEPRECATED\\|NOCOMMIT\\)"
 	1 font-lock-warning-face t))))
 
-;; global settings
-(setq query-replace-interactive t)		  ; Use last incremental seach regexp for query in regexp-query-replace
 (prefer-coding-system 'utf-8-auto-unix)
-(setq inhibit-startup-screen t
-      inhibit-splash-screen t)
-(setq recentf-max-menu-items 20)
-
 (set-frame-font "Menlo-11")
-;; (set-frame-font "Menlo Regular-11")
-;; (set-frame-font "Source Code Pro-11")
 
 (setq
- scroll-step 1                                    ; prevent Emacs from getting into weird state where it insists on centering the buffer on the cursor
-  -conservatively 9999
-  -up-aggresively 0.01
- scroll-down-aggresively 0.01
+ ac-auto-show-menu t                    	  ; automatically show menu
+ ac-quick-help-delay 0.5                	  ; shorter delay before showing quick help. Default is 1.5, 0 makes it crash
+ ac-sources '(ac-source-abbrev ac-source-dictionary ac-source-words-in-same-mode-buffers)
+ auto-revert-verbose nil
  auto-window-vscroll nil                          ; don't 'automatically adjust window to view tall lines'
- mouse-wheel-scroll-amount '(1 ((shift) . 1 ))
- scroll-margin 1
- whitespace-line-column 200                       ; don't highlight lines in whitespace mode unless they're REALLY giant. (default is 80)
- require-final-newline t                          ; add final newline on save
- initial-frame-alist (quote ((fullscreen . maximized)))
  bm-cycle-all-buffers t                           ; visual bookmarks bm-next and bm-previous should cycle all buffers
+ clean-buffer-list-delay-special 30
+ global-auto-revert-non-file-buffers t  	  ; also refresh dired but be quiet about it
+ inhibit-splash-screen t
+ inhibit-startup-screen t
+ initial-frame-alist (quote ((fullscreen . maximized)))
+ mouse-wheel-scroll-amount '(1 ((shift) . 1 ))
+ nav-width 30                      		  ; nav should be 30 chars wide (default is 18)
+ ns-right-command-modifier 'hyper
+ ns-right-control-modifier 'hyper
+ ns-right-option-modifier 'alt
+ query-replace-interactive t                      ; Use last incremental seach regexp for query in regexp-query-replace
+ recentf-max-menu-items 20
+ require-final-newline t                          ; add final newline on save
+ scroll-margin 1
+ w32-apps-modifier 'hyper
+ whitespace-line-column 200                       ; don't highlight lines in whitespace mode unless they're REALLY giant. (default is 80)
+ x-select-enable-clipboard t           		  ; Use the clipboard in addition to emacs kill ring
  )
-(setq global-auto-revert-non-file-buffers t)	  ; also refresh dired but be quiet about it
-(setq auto-revert-verbose nil)
-(setq ac-delay 0.1)				  ; shorter delay before showing completions. Default is 0.1.
-(setq ac-auto-show-menu t)			  ; automatically show menu
-(setq ac-quick-help-delay 0.5)			  ; shorter delay before showing quick help. Default is 1.5, 0 makes it crash
-(setq-default ac-sources '(ac-source-abbrev ac-source-dictionary ac-source-words-in-same-mode-buffers))
-(setq midnight-period 10)			  ; every 10 secs run clean-buffer-list, which kills *Help*, *Buffer List*, *Apropos*, etc buffers that haven't been visited recently
-(midnight-delay-set 'midnight-delay 10)		  ; Have to use this function to set midnight-delay
-(setq clean-buffer-list-delay-special 30)
-(setq clean-buffer-list-delay-general 0.02)	  ; Kill ANY buffer that hasn't been used in the half-hour (ish) - param is in day
-(setq clean-buffer-list-kill-regexps		  ; Remove all starred buffers not currently in use
-      '("\\*.*\\*"))
-(setq nav-width 30)				  ; nav should be 30 chars wide (default is 18)
-(nav-disable-overeager-window-splitting)	  ; turn off automatic splitting of frames when opening files in a large frame (?)
+
+(midnight-delay-set 'midnight-delay 10)           ; Have to use this function to set midnight-delay
 (set-default 'indent-tabs-mode nil) 		  ; Indentation can insert tabs if this is non-nil
-(setq x-select-enable-clipboard t)		  ; Use the clipboard in addition to emacs kill ring
+
+;; (setq
+;;  clean-buffer-list-kill-regexps		  ; Remove all starred buffers not currently in use
+;; )
+
+(defun switch-to-nav-buffer-other-window ()
+  "Switch to the *nav* buffer"
+  (interactive)
+  (switch-to-buffer-other-window "*nav*"))
 
 ;; custom key bindings
 (define-keys nil
-  '(("C-x u" nil) 				  ; disable emacs default keybinding for undo, use C-z instead
-    ("C-x C-b" buffer-menu)			  ; C-x C-b shows buffer menu
-    ("C-x C-r" recentf-open-files)		  ; C-x C-r -> display recent files (overrides open file in read-only mode)
-    ("C-x C-d" ido-dired)			  ; C-x C-d -> dired instead of list directory
-    ("C-M-S-k" backward-kill-sexp)		  ; C-M-S-k is backward-kill-sexp (kill-sexp is (C-M-k))
-    ("C-S-k" backward-kill-line)
-    ("C-M-y" popup-yank-menu)
-    ("C-x k" kill-this-buffer)			  ; kill-this-buffer instead of kill-buffer (prompts for which buffer)
-    ("<f9>" whitespace-mode)
-    ("<f10>" (lambda ()				  ; Jump to a nav buffer. F10 replaces menu-bar-open, which lets you browse menu from a buffer
-	       (interactive)
-	       (switch-to-buffer-other-window "*nav*")))
-    ("S-<f10>" nav)				  ; Open nav buffer
+  '(
+    ("<C-s-M-down>" windmove-down)
+    ("<C-s-M-left>" windmove-left)
+    ("<C-s-M-return>" other-frame)
+    ("<escape>" ace-jump-mode)
+    ("<f10>" switch-to-nav-buffer-other-window)   ; Jump to a nav buffer. F10 replaces menu-bar-open, which lets you browse menu from a buffer
     ("<f11>" paredit-mode)			  ; F11 is now global key for paredit-mode
-    ("<f12> s" stackoverflow-search)
     ("<f12> b" bing-search)
-    ;; ("C-z" undo)     ; C-z -> undo instead of minimize emacs ; C-z used by evil-mode to switch to emacs state
-    ("C-v" yank)				  ; C-v -> yank instead of whatever it usually does
-    ;; ("<escape>" keyboard-escape-quit)
+    ("<f12> s" stackoverflow-search)
+    ("<f13>" popup-cam-menu)
+    ("<f9>" whitespace-mode)
     ("<insert>" nil)			          ; disable stupid insert key TODO maybe use as a prefix to insert something useful
-    ("H-E" mc/mark-next-like-this)                ; Apparently Insert = Hyper on OS X WHEN USED IN COMBINATION WITH OTHER MODIFIER KEYS!
-    ("H-A" mc/mark-previous-like-this)
     ("C-H-a" mc/mark-all-like-this)
     ("C-H-e" mc/edit-lines)
-    ("H-h" highlight-symbol-at-point)
+    ("C-M-S-k" backward-kill-sexp)		  ; C-M-S-k is backward-kill-sexp (kill-sexp is (C-M-k))
+    ("C-M-y" popup-yank-menu)
+    ("C-S-k" backward-kill-line)
     ("C-c e" eval-and-replace)		     	  ; eval previous elisp expression at point, replace with results
-    ("M-j" join-next-line)
+    ("C-v" yank)				  ; C-v -> yank instead of whatever it usually does
+    ("C-x C-b" buffer-menu)			  ; C-x C-b shows buffer menu
+    ("C-x C-d" ido-dired)			  ; C-x C-d -> dired instead of list directory
+    ("C-x C-r" recentf-open-files)		  ; C-x C-r -> display recent files (overrides open file in read-only mode)
+    ("C-x k" kill-this-buffer)			  ; kill-this-buffer instead of kill-buffer (prompts for which buffer)
+    ("C-x u" nil) ; disable emacs default keybinding for undo, use C-z instead
     ("C-x z")				      	  ; disable minimize emacs
-    ("<f13>" popup-cam-menu)
-    ("s-]" force-indent-region)
+    ("H-A" mc/mark-previous-like-this)
+    ("H-E" mc/mark-next-like-this)                ; Apparently Insert = Hyper on OS X WHEN USED IN COMBINATION WITH OTHER MODIFIER KEYS!
+    ("H-h" highlight-symbol-at-point)
+    ("M-j" join-next-line)
+    ("S-<f10>" nav)				  ; Open nav buffer
     ("s-[" force-unindent-region)
-    ("s-o" ftf-find-file)
+    ("s-]" force-indent-region)
     ("s-f" ftf-grepsource)
-    ("<escape>" ace-jump-mode)
-    ("A-<tab>" ace-jump-buffer)
-    ("C-x C-g" keyboard-quit)                     ; Quit commands that I started typing with C-x
-    ("M-x" smex)				  ; smex is IDO-mode like M-x behavior
-    ;; ("M-ESC" helm-mini)
-    ("H-;" loccur-current)                        ; folder current buffer to lines containing the current word
-    ("A-;" loccur)                                ; activate loccur-mode (prompt for word/regex)
-    ("A-H-;" loccur-previous-match)               ; jump batch to previous loccur search
-    ("s-b" balance-windows)
-    ("A-b" bm-toggle)                             ; Toggle visual bookmark on this line
-    ("A-H-b" bm-show-all)                         ; Show all visual bookmarks in all files
-    ("A-n" bm-next)
-    ("A-p" bm-previous)
-    ))
-
-
-;; THE HOLY GRAIL <3
-(setq ns-right-control-modifier 'hyper
-      ns-right-command-modifier 'hyper
-      ns-right-option-modifier 'alt)
-
-(setq w32-apps-modifier 'hyper)
-(define-keys nil
-  '(("<H-up>" windmove-up)
-     ("<H-left>" windmove-left)
-    ("<H-right>" windmove-right)
-    ("<H-down>" windmove-down)
-    ("<C-s-M-left>" windmove-left)
+    ("s-o" ftf-find-file)
+    ;; ("C-z" undo)     ; C-z -> undo instead of minimize emacs ; C-z used by evil-mode to switch to emacs state
     ("<C-s-M-right>" windmove-right)
-    ("<C-s-right>" previous-buffer)
-    ("<C-s-left>" next-buffer)
     ("<C-s-M-up>" windmove-up)
-    ("<C-s-M-down>" windmove-down)
-    ("<H-SPC>" other-frame)
-    ("<C-s-M-return>" other-frame)
+    ("<C-s-left>" next-buffer)
+    ("<C-s-right>" previous-buffer)
     ("<H-S-left>" previous-buffer)
     ("<H-S-right>" next-buffer)
-    ("H-k" kill-this-buffer)))
+    ("<H-SPC>" other-frame)
+    ("<H-down>" windmove-down)
+    ("<H-left>" windmove-left)
+    ("<H-right>" windmove-right)
+    ("<H-up>" windmove-up)
+    ("A-;" loccur)                                ; activate loccur-mode (prompt for word/regex)
+    ("A-<tab>" ace-jump-buffer)
+    ("A-H-;" loccur-previous-match)               ; jump batch to previous loccur search
+    ("A-H-b" bm-show-all)                         ; Show all visual bookmarks in all files
+    ("A-b" bm-toggle)                             ; Toggle visual bookmark on this line
+    ("A-n" bm-next)
+    ("A-p" bm-previous)
+    ("C-x C-g" keyboard-quit)                     ; Quit commands that I started typing with C-x
+    ("H-;" loccur-current)                        ; folder current buffer to lines containing the current word
+    ("H-k" kill-this-buffer)
+    ("M-x" smex)				  ; smex is IDO-mode like M-x behavior
+    ("s-b" balance-windows)
+    ))
 
 (defun menu-edit-file (str f)
   (vector str (list 'lambda '() '(interactive) (list 'find-file f))))
@@ -293,18 +282,20 @@
 			"html-init.el"
 			"cpp-init.el")))))
 
-(mapc 'require '(lisp-init
+(mapc 'require '(
 		 clojure-init
+		 cpp-init
 		 elisp-init
-		 org-init
-		 js-init
-		 markdown-init
-		 ruby-init
-		 objc-init
-		 python-init
 		 erlang-init
 		 html-init
-		 cpp-init))
+		 js-init
+		 markdown-init
+		 objc-init
+		 org-init
+		 python-init
+		 ruby-init
+                 lisp-init
+                 ))
 
 (custom-set-variables
  '(custom-enabled-themes (quote (organic-green)))
