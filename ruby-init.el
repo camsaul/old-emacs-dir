fd45471171ccabbf82218a5330679c99e323b615
Cameron Saul <cameron@getluckybird.com>
2014-10-05 22:37:55 -0700

    Bind C-c C-f to ruby-insert-end function; ruby-insert-end inserts new line if needed

--- ruby-init.el
+++ ruby-init.el
@@ -21,11 +21,19 @@
 (setq ruby-block-highlight-toggle 'overlay) ; highlight ruby block on screen instead of minibuffer
 (setq ruby-block-delay 0.01) ; delay before showing matching block; default is 0.5
 
+(define-keys ruby-mode-map
+  '(("C-c C-f" ruby-insert-end)))
+
 ;; Apparently this function is missing from the version of ruby-electric on MELPA, although it attemps to call it;
 ;; work around http://stackoverflow.com/questions/10326255/emacs-ruby-electric-does-not-insert-end
 (defun ruby-insert-end ()
   "Insert \"end\" at point and reindent current line."
   (interactive)
+  (beginning-of-line)
+  (indent-for-tab-command)
+  (when (not (eolp))
+    (end-of-line)
+    (paredit-newline))
   (insert "end")
   (ruby-indent-line t)
   (end-of-line))
