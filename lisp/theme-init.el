0fef15f5fb18945f5322968d7de1017f8569eeb4
Cam Saul <cam@geotip.com>
2014-10-16 13:55:06 -0700

    Cleanup the powerline format a bit

--- lisp/theme-init.el
+++ lisp/theme-init.el
@@ -3,18 +3,14 @@
 
 ;;; Code:
 
-(require 'moe-theme)
-
 (set-frame-font
  (cdr (assoc system-type
              '((windows-nt . "Consolas-10") ; what about "gnu/linux" ?
                (darwin . "Menlo-11")))))
 
+(require 'moe-theme)
 (moe-light)
 
-(set-face-bold 'mode-line t)
-(set-face-background 'mode-line (face-background 'hl-line))
-(set-face-foreground 'mode-line "white")
 
 ;; Evil Mode Configuration
 
@@ -71,7 +67,7 @@
           (cam-toggle-minor-modes linum-mode relative-line-numbers-mode))
 
 (defmacro def-pl-faces (name bg-color fg-color &rest rest)
-  "Helper to create new face(s) via defface for powerline."
+  "Helper to create new face(s) via defface for powerline. Calls set-face-background/foreground as well."
   (let* ((bg-color (eval bg-color))
          (fg-color (eval fg-color))
          (bg (if (symbolp bg-color) (face-background bg-color)
@@ -90,16 +86,23 @@
        ,@(when rest
            (cdr (macroexpand `(def-pl-faces ,@rest))))))) ; cdr to skip the initial progn
 
-(symbolp 'region)
-
 (def-pl-faces
   pl-active-1 'region 'region
   pl-active-2 'secondary-selection 'secondary-selection
   pl-active-3 'hl-line "black"
-  pl-inactive-color-face "grey30" "grey90"
-  pl-inactive-1 "grey80" "grey20"
-  pl-inactive-2 "grey70" "grey10"
-  pl-inactive-3 "grey60" "black")
+  pl-inactive-color-face "gray30" "grey90"
+  pl-inactive-1 "gray45" "gray80"
+  pl-inactive-2 "gray60" "gray20"
+  pl-inactive-3 "gray75" "gray35"
+  mode-line 'hl-line "white"
+  mode-line-inactive "gray90" "gray50")
+
+(set-face-bold 'mode-line t)
+(set-face-bold 'mode-line-inactive nil)
+
+;; #5fafd7
+(pl/hex-color
+ (face-background 'region))
 
 (setq-default mode-line-format
   '("%e"
@@ -110,31 +113,27 @@
             (face2 (if active 'pl-active-2 'pl-inactive-2))
             (face3 (if active 'pl-active-3 'pl-inactive-3))
             (lhs (list
-                  (powerline-raw (powerline-evil-tag) color-face 'l)
-                  (powerline-raw " " color-face)
-                  (powerline-arrow-left color-face face1)
+                  (powerline-raw (concat (powerline-evil-tag) " ") color-face 'l)
+                  (powerline-slant-left color-face face1)
 
-                  (powerline-buffer-id face1 'l)
-                  (powerline-raw " " face1)
-                  (powerline-arrow-left face1 face2)
+                  (powerline-raw (concat " " (buffer-name) " ") face1 '1)
+                  (powerline-slant-left face1 face2)
 
                   (powerline-major-mode face2 'l)
                   (powerline-process face2)
                   (powerline-raw " " face2)
-                  (powerline-arrow-left face2 face3)
+                  (powerline-slant-left face2 face3)
 
                   (powerline-minor-modes face3 'l)
                   (powerline-narrow face3 'l)
                   (powerline-raw " " face3)))
 
-            (rhs (list (powerline-arrow-right face3 face1)
-                       (powerline-raw " " face1)
-                       (powerline-raw "%l" face1 'r)
-                       (powerline-arrow-right face1 color-face)
+            (rhs (list (powerline-slant-left face3 face1)
+                       (powerline-raw " %l" face1 'r)
+                       (powerline-slant-left face1 color-face)
 
                        (when global-mode-string
-                         (powerline-raw global-mode-string color-face)
-                         (powerline-raw " " color-face))
+                         (powerline-raw (concat global-mode-string " ") color-face))
                        (powerline-vc color-face 'r))))
        (concat (powerline-render lhs)
                (powerline-fill face3 (powerline-width rhs))
