1fdfe553d00db927b7c953a1e282442037bbe12a
Cam Saul <cam@getluckybird.com>
2015-03-31 23:44:19 -0700

    alias eshell clear -> cam/eshell-clear

--- lisp/eshell-init.el
+++ lisp/eshell-init.el
@@ -1,8 +1,16 @@
-;; -*- lexical-binding: t; comment-column: 60; -*-
+;; -*- lexical-binding: t; comment-column: 50; -*-
+
+;;; Commentary:
+;;; Just put at the end of .bash_profile:
+;;; emacsclient -nw --eval '(eshell)' || emacs -nw --eval '(progn (server-start) (eshell))'
+;;; <3
+
 ;;; Code:
 
-(mapc #'require '(eshell
-                  cam-macros))
+(require 'eshell)
+(require 'em-ls)
+(require 'cam-functions)
+(require 'cam-macros)
 
 (defun cam/eshell-clear ()
   "Clear previous text in eshell."
@@ -12,11 +20,17 @@
 
 (defun cam/eshell-setup ()
   (cam/define-keys eshell-mode-map
-    "C-c M-o" #'cam/eshell-clear))
+    "C-c M-o" #'cam/eshell-clear)
+
+  ;; Aliases
+  ;; I would imagine there's some better way to do this
+  (eshell-command "alias clear cam/eshell-clear"))
 
 (add-hook 'eshell-mode-hook #'cam/eshell-setup)
 
-(setq eshell-ls-initial-args '("-a"))                       ; list of args to pass to ls (default = nil)
+(setq eshell-ls-initial-args '("-a")              ; list of args to pass to ls (default = nil)
+      eshell-prefer-lisp-functions t              ; prefer built-in eshell commands to external ones
+      )
 
 (provide 'eshell-init)
 ;;; eshell-init.el ends here
