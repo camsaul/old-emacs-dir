d27877ced08c35ec74e5ab35a5f6e56b484f77c2
Cam Saül <cameron@getluckybird.com>
2015-04-20 14:29:31 -0700

    Bind C-c c to cam/org-insert-code-block in org-mode

--- lisp/org-init.el
+++ lisp/org-init.el
@@ -10,10 +10,25 @@
 ;; set global agenda files
 (setq org-support-shift-select nil)
 (cam/define-keys nil
-                 "C-c l" 'org-insert-link-global
-                 "C-c a t" 'org-todo-list
-                 "C-c a a" 'org-agenda
-                 "C-c a m" 'org-tags-view)
+  "C-c a t" #'org-todo-list
+  "C-c a a" #'org-agenda
+  "C-c a m" #'org-tags-view
+  "C-c l" #'org-insert-link-global)
+
+(cam/define-keys org-mode-map
+  "C-c c" #'cam/org-insert-code-block)
+
+(defun cam/org-insert-code-block (language)
+  "Insert a new Org code block and start editing it."
+  (interactive "sLanguage: ")
+  (org-return-indent)
+  (insert "#+BEGIN_SRC " language)
+  (org-return-indent)
+  (insert "#+END_SRC")
+  (forward-line -1)
+  (org-end-of-line)
+  (org-return-indent)
+  (org-edit-src-code))
 
 (provide 'org-init)
 ;;; org-init.el ends here
